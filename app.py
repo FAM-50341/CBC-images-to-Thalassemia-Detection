21452c54ce4e189410efead9afa84b812959746f
Farzana Akter Mily <aktermily698@gmail.com>
2025-08-02 18:32:52 +0600

    app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-import shap
-import lime
-import lime.lime_tabular
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pytesseract
